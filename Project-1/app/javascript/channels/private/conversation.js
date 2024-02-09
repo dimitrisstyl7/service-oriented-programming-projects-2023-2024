492fe58753eaa5043f34a487ddcb0b7a3ab3029c
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-09 21:12:07 +0200

    Create a unique private conversation channel and subscribe to it #10

---
+++ Project-1/app/javascript/channels/private/conversation.js
@@ -0,0 +1,8 @@
+App.private_conversation = App.cable.subscriptions.create("Private::ConversationChannel", {
+    connected: function () {
+    },
+    disconnected: function () {
+    },
+    received: function (data) {
+    }
+});
