ccb687b5238670685afaa3d256d36052b9c8cc3d
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-09 18:51:56 +0200

    Render messages with AJAX #10
    - Create a _message.html.erb inside private/messages.
    - Define a private_message_date_check helper method in Private::MessagesHelper and write specs for it.
    - Create a _new_date.html.erb inside private/messages/message.
    - Define sent_or_received and seen_or_unseen helper methods in Private::MessagesHelper and write specs for them.
    - Create a _load_more_messages.js.erb inside private/messages.
    - Define an append_previous_messages_partial_path helper method in Shared::MessagesHelper.
    - Create a _append_messages.js.erb inside shared/load_more_messages/window.
    - Define a replace_link_to_private_messages_partial_path in Private::MessagesHelper.
    - Create a _add_link_to_messages.js.erb inside private/messages/load_more_messages/window.
    - Create a toggle_window.js inside javascript/conversations.
    - Create a messages_infinite_scroll.js inside javascript/conversations.
    - Define an index action inside the Private::MessagesController.
    - Create a messages.rb inside controllers/concerns.
    - Define a remove_link_to_messages inside helpers/shared.
    - Create a _remove_more_messages_link.js.erb inside shared/load_more_messages/window.

--- Project-1/app/helpers/application_helper.rb
+++ Project-1/app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
   include NavigationHelper
   include PostsHelper
   include Private::ConversationsHelper
+  include Private::MessagesHelper
 
   def private_conversations_windows
     params[:controller] != 'messengers' ? @private_conversations_windows : []
