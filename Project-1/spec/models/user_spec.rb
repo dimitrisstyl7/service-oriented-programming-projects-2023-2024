174fd509495d889e3bc919f914837be47c188e92
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-10 21:43:37 +0200

    Create Group::Conversation and Group::Message models #12
    - Define associations.
    - Write specs.

--- Project-1/spec/models/user_spec.rb
+++ Project-1/spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   context 'Associations' do
     it 'has_many posts' do
       association = described_class.reflect_on_association(:posts)
@@ -47,6 +46,18 @@ RSpec.describe User, type: :model do
       expect(association.macro).to eq :has_many
       expect(association.options[:through]).to eq :all_received_contact_requests
       expect(association.options[:source]).to eq :user
+    end
+
+    it 'has_many group_messages' do
+      association = described_class.reflect_on_association(:group_messages)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:class_name]).to eq 'Group::Message'
+    end
+
+    it 'has_and_belongs_to_many group_conversations' do
+      association = described_class.reflect_on_association(:group_conversations)
+      expect(association.macro).to eq :has_and_belongs_to_many
+      expect(association.options[:class_name]).to eq 'Group::Conversation'
     end
   end
 
