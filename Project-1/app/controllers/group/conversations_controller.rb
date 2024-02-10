a3c689a7461b7e6f19b6e7951af83ae37ec413a0
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-10 22:09:32 +0200

    Update Group::ConversationsController #12
    - Create add_to_conversations method.
    - Create create_group_conversation and already_added? private methods.

--- Project-1/app/controllers/group/conversations_controller.rb
+++ Project-1/app/controllers/group/conversations_controller.rb
@@ -7,4 +7,23 @@ class Group::ConversationsController < ApplicationController
       format.js
     end
   end
+
+  private
+
+  def add_to_conversations
+    session[:group_conversations] ||= []
+    session[:group_conversations] << @conversation.id
+  end
+
+  def already_added?
+    session[:group_conversations].include?(@conversation.id)
+  end
+
+  def create_group_conversation
+    Group::NewConversationService.new({
+                                        creator_id: params[:creator_id],
+                                        private_conversation_id: params[:private_conversation_id],
+                                        new_user_id: params[:group_conversation][:id]
+                                      }).call
+  end
 end
