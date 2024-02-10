57209f96cfd312bf72f3a25ee2abbf23a48b409b
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-10 22:07:20 +0200

    Define a create action endpoint in Group::ConversationsController #12

--- Project-1/app/controllers/group/conversations_controller.rb
+++ Project-1/app/controllers/group/conversations_controller.rb
@@ -1,2 +1,10 @@
 class Group::ConversationsController < ApplicationController
+  def create
+    @conversation = create_group_conversation
+    add_to_conversations unless already_added?
+
+    respond_to do |format|
+      format.js
+    end
+  end
 end
