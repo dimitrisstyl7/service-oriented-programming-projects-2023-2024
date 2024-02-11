a9fb82a2e1971aef4a429ae0fc4baf2a8d57cac8
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-11 21:18:52 +0200

    Move some specs from spec\helpers\shared\conversations_helper_spec.rb to spec\helpers\group\conversations_helper_spec.rb #12

--- Project-1/spec/helpers/shared/conversations_helper_spec.rb
+++ Project-1/spec/helpers/shared/conversations_helper_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-include Group::ConversationsHelper
 
 RSpec.describe Shared::ConversationsHelper, :type => :helper do
   context '#private_conv_seen_status' do
@@ -63,31 +62,6 @@ RSpec.describe Shared::ConversationsHelper, :type => :helper do
       conversation.messages << message
       allow(helper).to receive(:current_user).and_return(user)
       expect(helper.group_conv_seen_status(conversation, user)).to eq ''
-    end
-  end
-
-  context '#add_people_to_group_conv_list' do
-    let(:conversation) { create(:group_conversation) }
-    let(:current_user) { create(:user) }
-    let(:user) { create(:user) }
-    before(:each) do
-      create(:contact,
-             user_id: current_user.id,
-             contact_id: user.id,
-             accepted: true)
-    end
-
-    it 'a user is not included in a list' do
-      conversation.users << current_user
-      conversation.users << user
-      allow(helper).to receive(:current_user).and_return(current_user)
-      expect(helper.add_people_to_group_conv_list(conversation)).not_to include user
-    end
-
-    it 'a user is included in a list' do
-      conversation.users << current_user
-      allow(helper).to receive(:current_user).and_return(current_user)
-      expect(helper.add_people_to_group_conv_list(conversation)).to include user
     end
   end
 end
