ed264fb004c2d246f5fb566ad6b77f3a40a890aa
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-12 17:13:45 +0200

    Define group_conversations_windows helper method inside the ApplicationHelper and create specs for ApplicationHelper's methods #13

--- Project-1/app/helpers/application_helper.rb
+++ Project-1/app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def private_conversations_windows
     params[:controller] != 'messengers' ? @private_conversations_windows : []
   end
+
+  def group_conversations_windows
+    params[:controller] != 'messengers' ? @group_conversations_windows : []
+  end
 end
