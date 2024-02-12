be5b32de8bea2374866e29eb1d4065490083c90f
dimitrisstyl7 <dimitrisstylianou@proton.me>
2024-02-12 17:34:02 +0200

    Create javascript\messenger.js #13

---
+++ Project-1/app/javascript/messenger.js
@@ -0,0 +1,9 @@
+$(document).on('turbo:load ajax:complete', function () {
+    const messages_visible = $('.conversation .messages-list ul', this).has('li').length;
+    const previous_messages_exist = $('.conversation .messages-list .load-more-messages', this).length;
+    // Load previous messages if messages list is empty && scrollbar doesn't exist
+    if (!messages_visible && previous_messages_exist) {
+        $('.load-more-messages', this)[0].click();
+        $('.conversation .messages-list .loading-more-messages').hide();
+    }
+});
